091547890221e344aede89efe522ffd0076245f8
Gautam99-coder <186465364+Gautam99-coder@users.noreply.github.com>
2025-04-02 00:39:32 +0530

    config.php

--- Your_Ad/config.php
+++ Your_Ad/config.php
@@ -1,1 +1,18 @@
 
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ad_platform";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
